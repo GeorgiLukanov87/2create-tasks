191431eb6da64574abd87abd35b3eab8e084f7da
Georgi St. Lukanov <102332504+GeorgiLukanov87@users.noreply.github.com>
2024-02-05 00:12:10 +0200

    Add files via upload

--- Task2/script.js
+++ Task2/script.js
@@ -51,27 +51,28 @@
 window.addEventListener('DOMContentLoaded', () => {
     const boxCollection = Array.from(document.querySelectorAll('.container div'));
     let board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-    let playerTurn = true;  // Added variable to track player's turn
+    let playerTurn = true;
 
     boxCollection.forEach((box) => {
         box.addEventListener('click', play);
     });
 
     function play(e) {
-        if (!playerTurn) return;  // Exit if it's not the player's turn
+        if (!playerTurn)
+            return;
 
-        const currentBox = e.currentTarget;
+        let currentBox = e.currentTarget;
 
         if (!currentBox.style.backgroundColor && !currentBox.classList.contains('closed')) {
             currentBox.style.backgroundColor = 'red';
             currentBox.classList.add('closed');
 
-            const currentIndex = parseInt(currentBox.classList.value.split(' ')[1].split('-')[1]);
-            const removedIndex = board.indexOf(currentIndex);
+            let currentIndex = parseInt(currentBox.classList.value.split(' ')[1].split('-')[1]);
+            let removedIndex = board.indexOf(currentIndex);
 
             board.splice(removedIndex, 1);
 
-            playerTurn = false;  // Set playerTurn to false after the player's move
+            playerTurn = false;
 
             let randomIndex;
             for (let i = 0; i <= board.length; i++) {
@@ -79,14 +80,16 @@ window.addEventListener('DOMContentLoaded', () => {
                     continue;
                 } else {
                     randomIndex = Math.floor(Math.random() * board.length);
-                    const nextMoveIndex = board[randomIndex];
-                    const nextMove = boxCollection[nextMoveIndex];
+                    let nextMoveIndex = board[randomIndex];
+                    let nextMove = boxCollection[nextMoveIndex];
 
                     setTimeout(() => {
+                        if (!nextMove) return;
+
                         nextMove.style.backgroundColor = 'green';
                         nextMove.classList.add('closed');
                         board.splice(randomIndex, 1);
-                        playerTurn = true;  // Set playerTurn to true after the computer's move
+                        playerTurn = true;
                     }, 500);
                     return;
                 }
